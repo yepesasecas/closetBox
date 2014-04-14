9b4999285ecc31f5b37b2a2bd246e5e1071c9274
@yepesasecas <yepes07@gmail.com>
2014-04-14 14:27:21 -0500

    formulario de contactos

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,9 @@
 class ContactsController < ActionController::Base
 
 	def new
-
+    contact = Contact.new name: params[:name], email: params[:email]
+    if contact.save
+      redirect_to root_path, notice: "Bienvenido a ClosetBox. proximamente nos pondremos en contacto."
+    end
 	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140408043622) do
+ActiveRecord::Schema.define(version: 20140414191315) do
+
+  create_table "contacts", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "roles", force: true do |t|
     t.string   "name"
