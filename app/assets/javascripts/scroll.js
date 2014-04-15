29f8e4ceb74861b0888a33279c8200c1221b14a2
@yepesasecas <yepes07@gmail.com>
2014-04-15 13:07:21 -0500

    scrollit landing

---
+++ app/assets/javascripts/scroll.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $( '.scrollit' ).on('click', function(event) {
+    event.preventDefault();
+    var target = $(this).data('target');
+    $('html, body').animate({
+        scrollTop: $(target).offset().top - 51
+    }, 1000);
+  });
+});
