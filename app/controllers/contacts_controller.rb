d5c844125b6dad3977bcae8bb30f6043c15b943e
@yepesasecas <yepes07@gmail.com>
2014-04-15 17:52:20 -0500

    fonts workiing, modal window

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,9 +1,11 @@
 class ContactsController < ActionController::Base
 
 	def new
-    contact = Contact.new name: params[:name], email: params[:email]
-    if contact.save
-      redirect_to root_path, notice: "Bienvenido a ClosetBox. proximamente nos pondremos en contacto."
+    @contact = Contact.new name: params[:name], email: params[:email]
+    if @contact.save
+      @message = "¡Bienvenido a ClosetBox!"
+    else
+      @message = "¡Tu email ya se encuentra registrado!"
     end
 	end
 end
