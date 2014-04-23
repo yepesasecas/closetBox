2df71e04a0017efb7d94a12b31e9908cefa15d49
@yepesasecas <yepes07@gmail.com>
2014-04-23 13:03:12 -0500

    Modal message beauty

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,7 +3,7 @@ class ContactsController < ActionController::Base
 	def new
     @contact = Contact.new name: params[:name], email: params[:email]
     if @contact.save
-      @message = "¡Bienvenido a ClosetBox!"
+      @message = "¡Bienvenido a ClosetBox! Te estaremos contactando en los proximos dias"
     else
       @message = "¡Tu email ya se encuentra registrado!"
     end
